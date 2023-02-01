0d6f5120341fcd2818f1a691636949294a01ea5e
Michael_WI <50377597+pixelwaster@users.noreply.github.com>
2023-02-01 15:36:10 -0600

    Arduino linker fail

---
+++ object _files/test_udsu.ino.cpp.d
@@ -0,0 +1,16 @@
+C:\MyBuilds\sketch\test_udsu.ino.cpp.o: \
+ C:\MyBuilds\sketch\test_udsu.ino.cpp \
+ C:\Users\Michael\AppData\Local\Arduino15\packages\lgt8fx\hardware\avr\2.0.0\cores\lgt8f/Arduino.h \
+ C:\Users\Michael\AppData\Local\Arduino15\packages\lgt8fx\hardware\avr\2.0.0\cores\lgt8f/binary.h \
+ C:\Users\Michael\AppData\Local\Arduino15\packages\lgt8fx\hardware\avr\2.0.0\cores\lgt8f/WCharacter.h \
+ C:\Users\Michael\AppData\Local\Arduino15\packages\lgt8fx\hardware\avr\2.0.0\cores\lgt8f/WString.h \
+ C:\Users\Michael\AppData\Local\Arduino15\packages\lgt8fx\hardware\avr\2.0.0\cores\lgt8f/HardwareSerial.h \
+ C:\Users\Michael\AppData\Local\Arduino15\packages\lgt8fx\hardware\avr\2.0.0\cores\lgt8f/Stream.h \
+ C:\Users\Michael\AppData\Local\Arduino15\packages\lgt8fx\hardware\avr\2.0.0\cores\lgt8f/Print.h \
+ C:\Users\Michael\AppData\Local\Arduino15\packages\lgt8fx\hardware\avr\2.0.0\cores\lgt8f/Printable.h \
+ C:\Users\Michael\AppData\Local\Arduino15\packages\lgt8fx\hardware\avr\2.0.0\cores\lgt8f/USBAPI.h \
+ C:\Users\Michael\AppData\Local\Arduino15\packages\lgt8fx\hardware\avr\2.0.0\cores\lgt8f/Arduino.h \
+ C:\Users\Michael\AppData\Local\Arduino15\packages\lgt8fx\hardware\avr\2.0.0\variants\lgt8fx8p/pins_arduino.h \
+ C:\Users\Michael\AppData\Local\Arduino15\packages\lgt8fx\hardware\avr\2.0.0\cores\lgt8f/lgtx8p.h \
+ c:\users\michael\appdata\local\arduino15\packages\lgt8fx\hardware\avr\2.0.0\variants\standard\pins_arduino.h \
+ C:\Users\Michael\AppData\Local\Arduino15\packages\lgt8fx\hardware\avr\2.0.0\cores\lgt8f/fastio_digital.h
---
+++ object _files/udsu.S.d
@@ -0,0 +1,1 @@
+C:\MyBuilds\sketch\udsu.S.o: C:\MyBuilds\sketch\udsu.S
