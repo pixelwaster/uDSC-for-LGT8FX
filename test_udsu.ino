083c9d9e7e541f2b8cc806e4202a63e6ad5696f9
Michael_WI <50377597+pixelwaster@users.noreply.github.com>
2023-04-01 09:13:13 -0500

    Update test_udsu.ino
    fixed bad code

--- test_udsu.ino
+++ test_udsu.ino
@@ -2,7 +2,7 @@
  * lgt8f328p dsu test
  *
  */ 
-
+#include <Arduino.h>
 #include "udsu.h"
 
 int array_dx[] = {
@@ -20,20 +20,29 @@ int array_dy[] = {
 };
 
 
-volatile unsigned int  uint16 = 0;
-volatile signed int     int16 = 0;
-volatile unsigned long uint32 = 0;
-volatile signed long    int32 = 0;
+volatile unsigned int  an_uint16 = 0;
+volatile signed int     an_int16 = 0;
+volatile unsigned long an_uint32 = 0;
+volatile signed long    an_int32 = 0;
 volatile long int      timer_uDSC0,timer_uDSC1 = 0;
 volatile long int      timer_norm0,timer_norm1 = 0;
+volatile unsigned int xadd;
+volatile unsigned int  dy;
+volatile unsigned int  dx;
+unsigned char r20;
+int *Z;
+int *Y;
 
 
-void setup() {
+
+int main() {
+	//void setup()
+
     Serial.begin(115200);
     while (!Serial) { }
-}
+
 	
-	void loop() {
+	//void loop()
 	
 	Serial.println("uDSC Test");
 	Serial.println("---------");
@@ -44,85 +53,85 @@ void setup() {
 
 	timer_norm0 = millis();
 
-	uint16 = 1000 + 3111;
-	if(uint16 != 4111) goto error;
+	an_uint16 = 1000 + 3111;
+	if(an_uint16 != 4111) goto error;
 	
-	int16 = -1000 + -3111;
-	if(int16 != -4111) goto error;
+	an_int16 = -1000 + -3111;
+	if(an_int16 != -4111) goto error;
 	
-	uint32 = 0x70000000 + 0x1000;
-	if(uint32 != 0x70001000) goto error;
+	an_uint32 = 0x70000000 + 0x1000;
+	if(an_uint32 != 0x70001000) goto error;
 	
-	uint32 = 20 * 300;	
-	if(uint32 != 6000) 	goto error;
+	an_uint32 = 20 * 300;	
+	if(an_uint32 != 6000) 	goto error;
 
-	int32 = -20 * 300;
-	if(int32 != -6000) goto error;
+	an_int32 = -20 * 300;
+	if(an_int32 != -6000) goto error;
 	
-	int32 = -20 * 100;
-	if(int32 != -2000) goto error;
+	an_int32 = -20 * 100;
+	if(an_int32 != -2000) goto error;
 	
-	int32 = -20 * -200;
-	if(int32 != 4000) goto error;
+	an_int32 = -20 * -200;
+	if(an_int32 != 4000) goto error;
 	
-	uint32 = 40 * 300;
-	if(uint32 != 12000) goto error;
+	an_uint32 = 40 * 300;
+	if(an_uint32 != 12000) goto error;
 		
-	uint32 = 1000 + (40 * 300);
-	if(uint32 != 13000) goto error;		
+	an_uint32 = 1000 + (40 * 300);
+	if(an_uint32 != 13000) goto error;		
 		
-	uint32 = uint32 + (20 * 500);
-	if(uint32 != 23000) goto error;
+	an_uint32 = an_uint32 + (20 * 500);
+	if(an_uint32 != 23000) goto error;
 	
-	uint32 = sq(200);
-	if(uint32 != 40000) goto error;
+	an_uint32 = sq(200);
+	if(an_uint32 != 40000) goto error;
 	
-	uint32 = sq(300);
-	if(uint32 != 90000) goto error;
+	an_uint32 = sq(300);
+	if(an_uint32 != 90000) goto error;
 	
-	uint32 = 0; // dsu_clr()
-	uint32 = uint32;  // dsu_da()
-	if(uint32 != 0) goto error;
+	an_uint32 = 0; // dsu_clr()
+	an_uint32 = an_uint32;  // dsu_da()
+	if(an_uint32 != 0) goto error;
 	
-	uint32 = 0x00 + 0x400;
-	if(uint32 != 0x400) goto error;
+	an_uint32 = 0x00 + 0x400;
+	if(an_uint32 != 0x400) goto error;
 	
-	int32 = uint32 * -1;
-	if(int32 != -0x400) goto error;
+	an_int32 = an_uint32 * -1;
+	if(an_int32 != -0x400) goto error;
 	
-	uint32 = abs(uint32);
-	if(uint32 != 0x400) goto error;
+	an_uint32 = abs(an_uint32);
+	if(an_uint32 != 0x400) goto error;
 	
-	int xadd = 0 + 0;  // dsu_xadd(0, 0) best guess
-	uint32 = xadd;     // dsu_da() 
-	if(uint32 != 0) goto error;
+	xadd = 0 + 0;  // dsu_xadd(0, 0) best guess
+	an_uint32 = xadd;     // dsu_da() 
+	if(an_uint32 != 0) goto error;
 	
-	uint32 = 33333/11111; //dsu_div1 da=da/dy, dy=da%dy
-	uint16_t dy = 33333%11111;
-	if(uint32 != 3) goto error;
+	an_uint32 = 33333/11111; //dsu_div1 da=da/dy, dy=da%dy
+	dy = 33333%11111;
+	if(an_uint32 != 3) goto error;
 	
-	uint16 = dy; //dsu_dy();
-	if(uint16 != 0) goto error;
+	an_uint16 = dy; //dsu_dy();
+	if(an_uint16 != 0) goto error;
 	
-	uint32 = 5666/1111;
+	an_uint32 = 5666/1111;
 	dy = 5666%1111;
-	if(uint32 != 5) goto error;
+	if(an_uint32 != 5) goto error;
 	
-	uint16 = dy;
-	if(uint16 != 111) goto error;
+	an_uint16 = dy;
+	if(an_uint16 != 111) goto error;
 	
 	// clean up dsu status
-	uint32 = 0; // dsu_clr()	
+	an_uint32 = 0; // dsu_clr()	
 
 	// add 0x2000 offset to map to 16bit SRAM
 	//int32 = dsu_fmacss(((uint16_t)array_dx | 0x2000), ((uint16_t)array_dy | 0x2000), 32);
          // the 0x2000 adress offset not used
 
-	uint16_t dx = 0;
+	dx = 0;
 	dy = 0;
-	int *Z = array_dx; // dx_addr load to Z
-	int *Y = array_dy; // dy_addr load to Y
-	unsigned char r20=32;     //loop count is in r20
+	Z = &array_dx[0]; // dx_addr load to Z
+	Y = &array_dy[0]; // dy_addr load to Y
+	r20 = 32;     //loop count is in r20
 	fmacss2_loop:
 		// ldd r0, Z+2	
 			dx = *Z; // DX = *(dx_addr)
@@ -131,17 +140,17 @@ void setup() {
 			dy = *Y; // DY = *(dy_addr), then 
 			Y += 2;    // dy_addr += 2 (for 16bit address)
 		// out DSIR, r20  da += dx * dy
-		int32 += (dx * dy);
+		an_int32 += (dx * dy);
 		--r20;
 		if (r20 > 0) goto fmacss2_loop; //	breq fmacss2_done; rjmp fmacss2_loop;
 		                                 // condensed into one IF
 //	if (int32 != ) goto error;
 
-	
-	// test done
-	timer_norm1 = millis();
-	Serial.print ("Time for uDSC test:");
-	Serial.println (timer_norm0 - timer_norm1);	
+		
+// test done
+timer_norm1 = millis();
+Serial.print ("Time for uDSC test:");
+Serial.println (timer_norm0 - timer_norm1);	
 
 
 // ============= uDSC =========================	
@@ -151,77 +160,77 @@ void setup() {
 	// initial uDSU module to fast io addressing mode
 	dsu_init(DSU_MM_FAST); // 0x00
 	
-	uint16 = dsu_xadd(1000, 3111);
-	if(uint16 != 4111) goto error;
+	an_uint16 = dsu_xadd(1000, 3111);
+	if(an_uint16 != 4111) goto error;
 	
-	int16 = dsu_xads(-1000, -3111);
-	if(int16 != -4111) goto error;
+	an_int16 = dsu_xads(-1000, -3111);
+	if(an_int16 != -4111) goto error;
 	
-	uint32 = dsu_aday(0x70000000, 0x1000);
-	if(uint32 != 0x70001000) goto error;
+	an_uint32 = dsu_aday(0x70000000, 0x1000);
+	if(an_uint32 != 0x70001000) goto error;
 	
-	uint32 = dsu_xmuluu(20, 300);	
-	if(uint32 != 6000) 	goto error;
+	an_uint32 = dsu_xmuluu(20, 300);	
+	if(an_uint32 != 6000) 	goto error;
 
-	int32 = dsu_xmulsu(-20, 300);
-	if(int32 != -6000) goto error;
+	an_int32 = dsu_xmulsu(-20, 300);
+	if(an_int32 != -6000) goto error;
 	
-	int32 = dsu_xmulss(-20, 100);
-	if(int32 != -2000) goto error;
+	an_int32 = dsu_xmulss(-20, 100);
+	if(an_int32 != -2000) goto error;
 	
-	int32 = dsu_xmulss(-20, -200);
-	if(int32 != 4000) goto error;
+	an_int32 = dsu_xmulss(-20, -200);
+	if(an_int32 != 4000) goto error;
 	
-	uint32 = dsu_xmacuu1(0, 40, 300);
-	if(uint32 != 12000) goto error;
+	an_uint32 = dsu_xmacuu1(0, 40, 300);
+	if(an_uint32 != 12000) goto error;
 		
-	uint32 = dsu_xmacuu1(1000, 40, 300);
-	if(uint32 != 13000) goto error;		
+	an_uint32 = dsu_xmacuu1(1000, 40, 300);
+	if(an_uint32 != 13000) goto error;		
 		
-	uint32 = dsu_xmacuu0(20, 500);
-	if(uint32 != 23000) goto error;
+	an_uint32 = dsu_xmacuu0(20, 500);
+	if(an_uint32 != 23000) goto error;
 	
-	uint32 = dsu_usqx1(200);
-	if(uint32 != 40000) goto error;
+	an_uint32 = dsu_usqx1(200);
+	if(an_uint32 != 40000) goto error;
 	
-	uint32 = dsu_usqy1(300);
-	if(uint32 != 90000) goto error;
+	an_uint32 = dsu_usqy1(300);
+	if(an_uint32 != 90000) goto error;
 	
 	dsu_clr();
-	uint32 = dsu_da();
-	if(uint32 != 0) goto error;
+	an_uint32 = dsu_da();
+	if(an_uint32 != 0) goto error;
 	
-	uint32 = dsu_xadd(0, 0x400);
-	if(uint32 != 0x400) goto error;
+	an_uint32 = dsu_xadd(0, 0x400);
+	if(an_uint32 != 0x400) goto error;
 	
-	int32 = dsu_uneg2();
-	if(int32 != -0x400) goto error;
+	an_int32 = dsu_uneg2();
+	if(an_int32 != -0x400) goto error;
 	
-	uint32 = dsu_abs2();
-	if(uint32 != 0x400) goto error;
+	an_uint32 = dsu_abs2();
+	if(an_uint32 != 0x400) goto error;
 	
 	dsu_xadd(0, 0);
-	uint32 = dsu_da();
-	if(uint32 != 0) goto error;
+	an_uint32 = dsu_da();
+	if(an_uint32 != 0) goto error;
 	
-	uint32 = dsu_div1(33333, 11111);
-	if(uint32 != 3) goto error;
+	an_uint32 = dsu_div1(33333, 11111);
+	if(an_uint32 != 3) goto error;
 	
-	uint16 = dsu_dy();
-	if(uint16 != 0) goto error;
+	an_uint16 = dsu_dy();
+	if(an_uint16 != 0) goto error;
 	
-	uint32 = dsu_div1(5666, 1111);
-	if(uint32 != 5) goto error;
+	an_uint32 = dsu_div1(5666, 1111);
+	if(an_uint32 != 5) goto error;
 	
-	uint16 = dsu_dy();
-	if(uint16 != 111) goto error;
+	an_uint16 = dsu_dy();
+	if(an_uint16 != 111) goto error;
 	
 	// clean up dsu status
 	dsu_clr();	
 
 	// add 0x2000 offset to map to 16bit SRAM
-	int32 = dsu_fmacss(((uint16_t)array_dx | 0x2000), ((uint16_t)array_dy | 0x2000), 32);
-//	if (int32 != ) goto error;
+	an_int32 = dsu_fmacss(((uint16_t)array_dx | 0x2000), ((uint16_t)array_dy | 0x2000), 32);
+//	if (an_int32 != ) goto error;
 	
 	// test done
 	timer_uDSC1 = millis();
@@ -231,12 +240,9 @@ void setup() {
 // test done
 	while(1);
 
+error:	{// if test failed!
+	Serial.print("ERROR");
+	while(1); }	
 		
-error:	// if test failed!
-	while(1)
-	{
-		Serial.print("ERROR");
-	}	
-	
-
+return(1);
 }
